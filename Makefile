ece88f7b4ed978443c16fbb15adbbd5d0e1fccfc
crazyoptimist <hey@crazyoptimist.net>
2023-04-10 03:02:53 -0500

    fix: permission of starship install, pass --yes

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 bootstrap:
 	cat ${PWD}/bashrc > ${HOME}/.bashrc
 	cat ${PWD}/profile > ${HOME}/.profile
-	cat ${PWD}/tmux.conf > ${HOME}/.tmux.conf
+	sudo /bin/bash ${PWD}/starship/install.sh
 	cat ${PWD}/vimrc > ${HOME}/.vimrc
-	${PWD}/starship/install.sh
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	cat ${PWD}/tmux.conf > ${HOME}/.tmux.conf
+	mkdir -p ${HOME}/.config
+	cp ${PWD}/starship/starship.toml ${HOME}/.config/
--- starship/install.sh
+++ starship/install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 echo "Installing a nerd font named Hack, you can download another one from https://www.nerdfonts.com/font-downloads"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip
 unzip Hack.zip -d ~/.fonts
 fc-cache -fv
 echo "Installed a nerd font successfully!"
 
-## install emoji fonts package
-## for debian/ubuntu
+# Install emoji fonts package
+
+# Debian/Ubuntu
 sudo apt install fonts-noto-color-emoji
-## for centos/fedora
+
+# Centos/Fedora
 # dnf install google-noto-emoji-fonts
-## for arch linux:
+
+# Arch
 # pacman -S noto-fonts-emoji
 
-# install starship
-sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+# Install starship
+curl -sS https://starship.rs/install.sh | sudo sh -s -- --yes
 starship --version
 
 echo "Add following line to your .bashrc to use starship:"
