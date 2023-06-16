a8c8a39625b4e3438822530941a1d9c58f101167
crazyoptimist <hey@crazyoptimist.net>
2023-06-16 04:25:49 -0500

    feat: get back to gruvbox

--- nvim/lua/plugin-configs/colorscheme.lua
+++ nvim/lua/plugin-configs/colorscheme.lua
@@ -1,1 +1,6 @@
-vim.cmd [[ colorscheme catppuccin-mocha ]]
+vim.o.background = "dark" -- or "light" for light mode
+require("gruvbox").setup({
+  contrast = "soft", -- can be "hard", "soft" or empty string
+  transparent_mode = true,
+})
+vim.cmd("colorscheme gruvbox")
