639882ff10779bcf1368a09d1a45e1f6d95d01c2
crazyoptimist <hey@crazyoptimist.net>
2023-09-11 14:35:43 -0500

    feat: opt to disable icons in nvim-tree

--- nvim/lua/plugin-configs/nvim-tree.lua
+++ nvim/lua/plugin-configs/nvim-tree.lua
@@ -4,7 +4,17 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup({
   view = {
     adaptive_size = true
-  }
+  },
+  -- Disable showing icons
+  -- renderer = {
+  --   icons = {
+  --     show = {
+  --       file = false,
+  --       folder = false,
+  --       folder_arrow = false
+  --     }
+  --   }
+  -- }
 })
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
