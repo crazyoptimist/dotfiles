9addb7fdef59cd1a24f16eaead10b995d25e3926
crazyoptimist <hey@crazyoptimist.net>
2024-01-11 13:30:33 -0600

    feat: add gitgutter.vim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,6 +16,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-commentary'
+  use 'airblade/vim-gitgutter'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-lualine/lualine.nvim'
