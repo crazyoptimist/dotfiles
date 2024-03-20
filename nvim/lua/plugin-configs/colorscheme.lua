c4209e45dce26640b53d2363f24c38166a0ff6a4
crazyoptimist <hey@crazyoptimist.net>
2024-03-20 18:14:55 -0500

    chore: no transparent vim, use hard gruvbox

--- nvim/lua/plugin-configs/colorscheme.lua
+++ nvim/lua/plugin-configs/colorscheme.lua
@@ -1,6 +1,5 @@
 vim.o.background = "dark" -- or "light" for light mode
 require("gruvbox").setup({
-  contrast = "soft", -- can be "hard", "soft" or empty string
-  transparent_mode = true,
+  contrast = "hard", -- can be "hard", "soft" or empty string
 })
 vim.cmd("colorscheme gruvbox")
