64ceb8f3eb37dcb2a8b90ab6cdcd4d8ae8faf635
crazyoptimist <hey@crazyoptimist.net>
2024-03-26 18:29:57 -0500

    chore: lineup lua in lspconfig file

--- nvim/lua/plugin-configs/lspconfig.lua
+++ nvim/lua/plugin-configs/lspconfig.lua
@@ -15,22 +15,21 @@ local keymap = vim.keymap -- for conciseness
 local opts = { noremap = true, silent = true }
 
 local on_attach = function(_, buffer)
-
   opts.buffer = buffer
 
   -- set keybinds
-  keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-  keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-  keymap.set("n", "ge", vim.diagnostic.open_float, opts) -- show diagnostics for line
-  keymap.set("n", "gn", vim.diagnostic.goto_next, opts) -- go to next diagnostics in the file
-  keymap.set("n", "gp", vim.diagnostic.goto_prev, opts) -- go to prev diagnostics in the file
-  keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-  keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-  keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+  keymap.set("n", "K", vim.lsp.buf.hover, opts)                          -- show documentation for what is under cursor
+  keymap.set("n", "gD", vim.lsp.buf.declaration, opts)                   -- go to declaration
+  keymap.set("n", "ge", vim.diagnostic.open_float, opts)                 -- show diagnostics for line
+  keymap.set("n", "gn", vim.diagnostic.goto_next, opts)                  -- go to next diagnostics in the file
+  keymap.set("n", "gp", vim.diagnostic.goto_prev, opts)                  -- go to prev diagnostics in the file
+  keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)      -- show lsp definitions
+  keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)       -- show definition, references
+  keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)  -- show lsp implementations
   keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-  keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-  keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-  keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+  keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)  -- see available code actions, in visual mode will apply to selection
+  keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)                -- smart rename
+  keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)                 -- mapping to restart lsp if necessary
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
