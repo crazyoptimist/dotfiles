fa7015dfd99e0c25974afdae08b4d5716f6b3501
crazyoptimist <hey@crazyoptimist.net>
2024-04-30 15:28:39 -0500

    feat: upkeep the docker install script

--- docker/install.sh
+++ docker/install.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-# upgrades the operating system
-sudo apt-get update && sudo apt-get upgrade -y
 
-# install the latest version of docker engine
-sudo apt remove docker docker-engine docker.io containerd runc  # purging the legacy version if exists
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io -y
-# install the latest version of docker-compose
-# curl -SL https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64 -o docker-compose
-curl -s https://api.github.com/repos/docker/compose/releases/latest \
-| grep -v ".sha256" \
-| grep browser_download_url \
-| grep "docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)" \
-| cut -d '"' -f 4 \
-| xargs curl -L -o docker-compose
-chmod +x docker-compose
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
-sudo mv docker-compose /usr/local/lib/docker/cli-plugins
-# add the current (non-root) user to docker group
+# Uninstall all conflicting packages:
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg --purge -y; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Add the current (non-root) user to the docker group
 sudo groupadd -f docker
 sudo usermod -aG docker $USER
+
 sudo docker -v
 sudo docker compose version
-echo -e "\e[32mReboot your machine please. \e[0m"
+echo "\e[32mReboot your machine please. \e[0m"
