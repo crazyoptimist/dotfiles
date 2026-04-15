ab1ea9704fb291b9b8df43f9549fe4bad5019b5b
crazyoptimist <me@crazyoptimist.net>
2026-04-15 10:34:11 -0500

    feat: post install script - docker

---
+++ docker/postinstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Colors
+GREEN="\e[32m"
+RED="\e[31m"
+RESET="\e[0m"
+
+# Check if Docker Engine is installed
+if command -v docker >/dev/null 2>&1; then
+    echo -e "${GREEN}Docker Engine is already installed on your system.${RESET}"
+else
+    echo -e "${RED}Docker Engine is not installed on your system.${RESET}"
+    echo -e "${RED}Please follow the official Docker documentation to install Docker Engine.${RESET}"
+    exit 1
+fi
+
+# Manage Docker as a non-root user
+sudo groupadd -f docker
+sudo usermod -aG docker "$USER"
+
+sudo docker -v
+sudo docker compose version
+
+echo -e "${GREEN}Setup complete. Please reboot your machine.${RESET}"
