1f9cf389baaab39ef7033150625a9c1d3458796e
crazyoptimist <me@crazyoptimist.net>
2026-08-22 22:42:16 +0000

    feat: add terraform-ls LSP and claudecode.nvim plugin
    Enable terraform-ls alongside existing language servers, and wire up
    coder/claudecode.nvim with commands and <leader>a keymaps for toggling,
    resuming, sending selections/buffers, and managing diffs.

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "gopls", "rust_analyzer", "pyright" }
+local servers = { "html", "cssls", "ts_ls", "gopls", "rust_analyzer", "pyright", "terraform-ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
